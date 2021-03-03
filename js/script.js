a0cbc306fe1b7020dea80052368cf3df8ce346ab
Andrea <b.a.benvenuti3@gmail.com>
2021-03-03 14:59:00 +0100

    Aggiunto elenco cognomi

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//  creo un array contenente i cognomi
+var arrayCognomiBool = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+// chiedo all'utente di inserire il proprio cognome
+do {
+  var cognomeNew = prompt("inserisci qui il tuo cognome").toUpperCase();
+} while (!isNaN(cognomeNew));
+// inserisco il nuovo cognome nell'array in ordine alfabetico
+arrayCognomiBool.push(cognomeNew);
+arrayCognomiBool.sort();
+//  stampo il mio array sotto forma di lista a schermo
+for (var i = 0; i < arrayCognomiBool.length; i++) {
+  document.getElementById('lista_array').innerHTML += "<li>" + arrayCognomiBool[i] + "</li>";
+}
+// indico la posizione del nuovo cognome all'interno dell'array ordinato alfabeticamente
+var posizione = (arrayCognomiBool.indexOf(cognomeNew)) + 1;
+console.log(posizione);
