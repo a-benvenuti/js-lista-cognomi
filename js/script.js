206bf2c706c71610d2ab880b8edb71248d7aa459
Andrea <b.a.benvenuti3@gmail.com>
2021-03-03 15:26:08 +0100

    Resa "solo" la prima lettera maiuscola del nuovo elemento

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 //  creo un array contenente i cognomi
 var arrayCognomiBool = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
 // chiedo all'utente di inserire il proprio cognome
 do {
-  var cognomeNew = prompt("inserisci qui il tuo cognome").toUpperCase();
+  var cognomeNew = prompt("inserisci qui il tuo cognome");
+  // rendo solo la prima lettera maiuscola
+  cognomeNew = cognomeNew.charAt(0).toUpperCase() + cognomeNew.slice(1);
 } while (!isNaN(cognomeNew));
+
 // inserisco il nuovo cognome nell'array in ordine alfabetico
 arrayCognomiBool.push(cognomeNew);
 arrayCognomiBool.sort();
+
 //  stampo il mio array sotto forma di lista a schermo
 for (var i = 0; i < arrayCognomiBool.length; i++) {
   document.getElementById('lista_array').innerHTML += "<li>" + arrayCognomiBool[i] + "</li>";
